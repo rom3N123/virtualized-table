66cb872126d9aa0de18645343d76298b42584808
Martynov <r.martynov@gk-adept.ru>
2022-05-22 20:47:06 +0300

    fix: fix useObservable import

--- src/components/VirtualizedTable/plugins/useTableSelectionMode/defaultGetTableSelectionModeProps.ts
+++ src/components/VirtualizedTable/plugins/useTableSelectionMode/defaultGetTableSelectionModeProps.ts
@@ -1,31 +1,34 @@
 import { TableSelectionModeInstanceProps } from './useInstance';
 import { TableInstance, PropGetter } from 'react-table';
-import useObservable from '../../../../hooks/useObservable';
+import { useObservable } from '../../../../hooks';
 
 export type TableSelectionModeRowProps = TableSelectionModeInstanceProps & {
-    isSelectionMode: boolean;
+	isSelectionMode: boolean;
 };
 
-const defaultGetTableSelectionModeProps: PropGetter<object, object> = (props, { instance }) => {
-    const {
-        enableTableSelectionMode,
-        disableTableSelectionMode,
-        changeTableSelectionMode,
-        isSelectionModeObservable,
-    } = instance as TableInstance & TableSelectionModeRowProps;
+const defaultGetTableSelectionModeProps: PropGetter<object, object> = (
+	props,
+	{ instance }
+) => {
+	const {
+		enableTableSelectionMode,
+		disableTableSelectionMode,
+		changeTableSelectionMode,
+		isSelectionModeObservable,
+	} = instance as TableInstance & TableSelectionModeRowProps;
 
-    const isSelectionMode = useObservable(isSelectionModeObservable);
+	const isSelectionMode = useObservable(isSelectionModeObservable);
 
-    return [
-        props,
-        {
-            enableTableSelectionMode,
-            disableTableSelectionMode,
-            changeTableSelectionMode,
-            isSelectionModeObservable,
-            isSelectionMode,
-        },
-    ];
+	return [
+		props,
+		{
+			enableTableSelectionMode,
+			disableTableSelectionMode,
+			changeTableSelectionMode,
+			isSelectionModeObservable,
+			isSelectionMode,
+		},
+	];
 };
 
 export default defaultGetTableSelectionModeProps;
